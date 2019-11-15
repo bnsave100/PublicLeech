f426e34e07b4f462196d2afd7fabf0b7b73af393
Arsalan <35004378+a092devs@users.noreply.github.com>
2019-11-15 23:19:04 +0530

    🤪

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -55,7 +55,7 @@ async def incoming_message_f(client, message):
         if not sagtus:
             await i_m_sefg.edit_text(err_message)
     else:
-        await i_m_sefg.edit_text("**FCUK**! wat have you entered. Please read /help")
+        await i_m_sefg.edit_text("Please read /help")
 
 
 async def incoming_youtube_dl_f(client, message):
