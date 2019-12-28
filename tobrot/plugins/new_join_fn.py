ac12a0512cf6687eeb51af34579221ab0755766a
Arsalan <35004378+a092devs@users.noreply.github.com>
2019-12-28 15:59:52 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -12,7 +12,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
 import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 from tobrot import (
     AUTH_CHANNEL
@@ -22,6 +22,7 @@ from tobrot import (
 async def new_join_f(client, message):
     chat_type = message.chat.type
     if chat_type != "private":
+        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
         # leave chat
         await client.leave_chat(
             chat_id=message.chat.id,
@@ -32,12 +33,12 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    # await message.reply_text("No one is gonna help you 🤣", quote=True)
+    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
     channel_id = str(AUTH_CHANNEL)[4:]
     message_id = 99
     # display the /help message
     await message.reply_text(
-        "Please read the <a href='https://t.me/c/1218126124/36'>instructions.</a>",
+        f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
         quote=True
     )
 
