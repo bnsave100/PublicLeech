a2eb9cc5b4e40ec3b57f0e838b3e2b35dd685a34
Arsalan <35004378+a092devs@users.noreply.github.com>
2019-12-28 15:41:24 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -39,7 +39,7 @@ async def youtube_dl_call_back(bot, update):
     if current_user_id != current_touched_user_id:
         await bot.answer_callback_query(
             callback_query_id=update.id,
-            text="who are you? 🤪",
+            text="who are you? 🤪🤔🤔🤔",
             show_alert=True,
             cache_time=0
         )
@@ -79,24 +79,22 @@ async def youtube_dl_call_back(bot, update):
     #
     youtube_dl_url = response_json.get("webpage_url")
     LOGGER.info(youtube_dl_url)
-    youtube_dl_title = str(response_json.get("title"))
-    if "/" in youtube_dl_title:
-        youtube_dl_title = youtube_dl_title.replace("/", "_")
-    custom_file_name = youtube_dl_title + \
-        "_" + youtube_dl_format + "." + youtube_dl_ext
+    #
+    custom_file_name = "%(title)s.%(ext)s"
+    # https://superuser.com/a/994060
     LOGGER.info(custom_file_name)
     #
     if "noyes.in" in youtube_dl_url or "tor.checker.in" in youtube_dl_url:
         await bot.edit_message_text(
             chat_id=update.message.chat.id,
-            text="<i>please do not abuse this <u>FREE</u> service</i> 🌚",
+            text="😡😡 <i>please do not abuse this <u>FREE</u> service</i> 🌚",
             message_id=update.message.message_id
         )
         return
     if "drive.google.com" in youtube_dl_url and youtube_dl_format != "source":
         await bot.edit_message_text(
             chat_id=update.message.chat.id,
-            text="<i>Please do not abuse this <u>FREE</u> service</i>",
+            text="<i>please do not abuse this <u>FREE</u> service</i>",
             message_id=update.message.message_id
         )
         return
@@ -106,14 +104,18 @@ async def youtube_dl_call_back(bot, update):
         chat_id=update.message.chat.id,
         message_id=update.message.message_id
     )
-    description = "@a092devsbot"
+    description = "@PublicLeech"
     if "fulltitle" in response_json:
         description = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
     #
-    tmp_directory_for_each_user = DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+    tmp_directory_for_each_user = os.path.join(
+        DOWNLOAD_LOCATION,
+        str(update.from_user.id)
+    )
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
+    download_directory = tmp_directory_for_each_user
     download_directory = os.path.join(tmp_directory_for_each_user, custom_file_name)
     command_to_exec = []
     if tg_send_type == "audio":
@@ -153,6 +155,7 @@ async def youtube_dl_call_back(bot, update):
     command_to_exec.append("--no-warnings")
     # command_to_exec.append("--quiet")
     command_to_exec.append("--restrict-filenames")
+    #
     if "hotstar" in youtube_dl_url:
         command_to_exec.append("--geo-bypass-country")
         command_to_exec.append("IN")
@@ -170,7 +173,7 @@ async def youtube_dl_call_back(bot, update):
     t_response = stdout.decode().strip()
     # LOGGER.info(e_response)
     # LOGGER.info(t_response)
-    ad_string_to_replace = "Please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
+    ad_string_to_replace = "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
     if e_response and ad_string_to_replace in e_response:
         error_message = e_response.replace(ad_string_to_replace, "")
         await bot.edit_message_text(
@@ -184,24 +187,18 @@ async def youtube_dl_call_back(bot, update):
         # os.remove(save_ytdl_json_path)
         end_one = datetime.now()
         time_taken_for_download = (end_one -start).seconds
-        dir_contents = os.listdir(tmp_directory_for_each_user)
-        dir_contents.sort()
-        download_directory = ""
-        if len(dir_contents) >= 1:
-            download_directory = os.path.join(tmp_directory_for_each_user, dir_contents[0])
-        else:
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                message_id=update.message.message_id,
-                text=t_response + "\n" + e_response
-            )
-            return False, None
-        file_size = os.stat(download_directory).st_size
+        dir_contents = len(os.listdir(tmp_directory_for_each_user))
+        # dir_contents.sort()
+        await bot.edit_message_text(
+            chat_id=update.message.chat.id,
+            message_id=update.message.message_id,
+            text=f"found {dir_contents} files"
+        )
         user_id = update.from_user.id
         #
         final_response = await upload_to_tg(
             update.message,
-            download_directory,
+            tmp_directory_for_each_user,
             user_id,
             {}
         )
@@ -229,7 +226,7 @@ async def youtube_dl_call_back(bot, update):
             message_to_send = mention_req_user + message_to_send
             message_to_send = message_to_send + "\n\n" + "#uploads"
         else:
-            message_to_send = "<i>FAILED</i> to upload files. 😞"
+            message_to_send = "<i>FAILED</i> to upload files. 😞😞"
         await update.message.reply_to_message.reply_text(
             text=message_to_send,
             quote=True,
